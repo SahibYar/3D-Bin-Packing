b585fea4d6ddd7482a45a3e6eebaa3bd6b445185
Sahib Yar <sahibyaar@hotmail.com>
2016-02-07 02:32:57 +0500

    added the correction for has_free_space() function by changing its return type to Point3D

--- 3D-Bin-Packing/Guillotine3D.cs
+++ 3D-Bin-Packing/Guillotine3D.cs
@@ -15,13 +15,16 @@ namespace _3D_Bin_Packing
         // Created a list of boxes for holding all the box objects
         public static Dictionary<String, Box> BoxList = new Dictionary<string, Box>();
 
-        // list of the splitted containers from the given containers.
+
+        // list of the splitted containers from the given containers
+        //Dictionary<Container id, list of sub containers splitted from it>
         Dictionary<String, List<Containers>> Split_Container_open_list = new Dictionary<string, List<Containers>>();
         Dictionary<String, List<Containers>> Split_Container_closed_list = new Dictionary<string, List<Containers>>();
 
         // list of all the boxes in the given container.
         Dictionary<String, List<Box>> Container_Containing_Boxes = new Dictionary<string, List<Box>>();
 
+        //sort the boxes in their descending volume
         List<KeyValuePair<String, Box>> sorted_box_List;
         
         
@@ -49,6 +52,8 @@ namespace _3D_Bin_Packing
         Fill the box with items in it not violating its maximum weight limit
         and other constraints
         */
+
+        // This funtion will try to put the best possible object which can be fitted in the given container
         void fill_container(String key) //here the key will be of the smallest available container.
         {
             // Still to open = false
@@ -65,7 +70,13 @@ namespace _3D_Bin_Packing
             {
                 if (!box.Value.IsPlaced)        // if the box is not yet placed.
                 {
-                    
+
+                    if (has_free_space(key, box.Value).X != float.NegativeInfinity &&
+                        has_free_space(key, box.Value).Y != float.NegativeInfinity &&
+                        has_free_space(key, box.Value).Z != float.NegativeInfinity)     // checks if the box could be contained in the given container.
+                    {
+
+                    }
                 }
                 //if this box can be contained in the container(splitted continers)
                 // with out violating the weight and other constraint then
@@ -83,7 +94,9 @@ namespace _3D_Bin_Packing
         }
 
 
-        Boolean has_free_space(String Container_Key, Box box)
+
+        // This functions checks if the given box can be contained in the container.
+        Point3D has_free_space(String Container_Key, Box box)
         {
             // list of all the splitted-containers of the given container-key
             List<Containers> temp_list = Split_Container_open_list[Container_Key];
@@ -100,35 +113,52 @@ namespace _3D_Bin_Packing
                         if (container.Height >= box.Height &&
                             container.Length >= box.Length &&
                             container.Width >= box.Width)
-                            return true;
+                            return container.Origin;
 
+                        //if X rotation is allowed.
                         else if (box.AllowedRotationsX &&
                             container.Width >= box.Height &&
                             container.Height >= box.Width &&
                             container.Length >= box.Length)
-                            return true;
+                            return container.Origin;
 
+                        //if Z rotation is allowed
                         else if (box.AllowedRotationsZ &&
                             container.Height >= box.Height &&
                             container.Width >= box.Length &&
                             container.Length >= box.Width)
-                            return true;
+                            return container.Origin;
 
+                        // if Y rotation is allowed
                         else if (box.AllowedRotationsY &&
                             container.Width >= box.Width &&
                             container.Length >= box.Height &&
                             container.Height >= box.Length)
-                            return true;
+                            return container.Origin;
                     }
                 }
+                /*
+                for merging
+                check all the split rectangles of the given container
 
+            the time complexity will be bigOh(n^2)
+            compare each container with all other container that If they have common
+            adjacent wall.
+            If yes
+            {
+                then calculate the maximum possible free container.
+                create one and split the remaining one.
+            }
+            if no
+                ignore it.
+             */
                 // check all the bottom containers after merging them
             }
             else if (box.TopOnly)
             {
                 foreach (Containers container in temp_list)
                 {
-                    //as the object is bottom only so it should be placed on xy-plane only
+                    //as the object is top only only so it should be placed on xy-plane +  only
                     if (container.Origin.Z + container.Height == ContainerList[Container_Key].Height
                         && container.volume() >= box.Volume()) //z-axis value should be zero.
                     {
@@ -136,28 +166,28 @@ namespace _3D_Bin_Packing
                         if (container.Height >= box.Height &&
                             container.Length >= box.Length &&
                             container.Width >= box.Width)
-                            return true;
+                            return container.Origin;
 
                         //along X-axis rotation is allowed
                         else if (box.AllowedRotationsX &&
                             container.Width >= box.Height &&
                             container.Height >= box.Width &&
                             container.Length >= box.Length)
-                            return true;
+                            return container.Origin;
 
                         //along Z-axis rotation is allowed
                         else if (box.AllowedRotationsZ &&
                             container.Height >= box.Height &&
                             container.Width >= box.Length &&
                             container.Length >= box.Width)
-                            return true;
+                            return container.Origin;
 
                         //along Y-axis rotation is allowed
                         else if (box.AllowedRotationsY &&
                             container.Width >= box.Width &&
                             container.Length >= box.Height &&
                             container.Height >= box.Length)
-                            return true;
+                            return container.Origin;
                     }
                 }
 
@@ -173,36 +203,41 @@ namespace _3D_Bin_Packing
                         if (container.Height >= box.Height &&
                             container.Length >= box.Length &&
                             container.Width >= box.Width)
-                            return true;
+                            return container.Origin;
 
                         //along X-axis rotation is allowed
                         else if (box.AllowedRotationsX &&
                             container.Width >= box.Height &&
                             container.Height >= box.Width &&
                             container.Length >= box.Length)
-                            return true;
+                            return container.Origin;
 
                         //along Z-axis rotation is allowed
                         else if (box.AllowedRotationsZ &&
                             container.Height >= box.Height &&
                             container.Width >= box.Length &&
                             container.Length >= box.Width)
-                            return true;
+                            return container.Origin;
 
                         //along Y-axis rotation is allowed
                         else if (box.AllowedRotationsY &&
                             container.Width >= box.Width &&
                             container.Length >= box.Height &&
                             container.Height >= box.Length)
-                            return true;
+                            return container.Origin;
                     }
                 }
                 //after mergin top_only containers
             }
 
             // if still not available.
-            return false;
+            Point3D invalid_point = new Point3D();
+            invalid_point.X = float.NegativeInfinity;
+            invalid_point.Y = float.NegativeInfinity;
+            invalid_point.Z = float.NegativeInfinity;
+            return invalid_point;
         }
+
         //This function rearrange the items in descending order of there volumes.
         void re_arranging_boxes()
         {
@@ -210,5 +245,7 @@ namespace _3D_Bin_Packing
             sorted_box_List.Sort((firstPair, nextPair) => ( firstPair.Value.Width * firstPair.Value.Height * firstPair.Value.Length).CompareTo (nextPair.Value.Length * nextPair.Value.Width * nextPair.Value.Height));
             sorted_box_List.Reverse();
         }
+
+
     }
 }
